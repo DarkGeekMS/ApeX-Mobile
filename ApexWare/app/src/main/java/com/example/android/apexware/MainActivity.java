8d4efda2fc933883b278731212ab064f339bbf59
[sha3er97] <37841787+sha3er97@users.noreply.github.com>
2019-03-19 02:00:48 +0200

    [new] added toggle password button for login and sign up forms

--- ApexWare/app/src/main/java/com/example/android/apexware/MainActivity.java
+++ ApexWare/app/src/main/java/com/example/android/apexware/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.android.apexware;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Button login;
     Button signup;
     Button forgot_pass;
+    ToggleButton toggle_btn;
+    EditText pass_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         signup = (Button) findViewById(R.id.signup_btn);
 
         forgot_pass = (Button) findViewById(R.id.forgot_pass_btn);
+
+        toggle_btn = (ToggleButton) findViewById(R.id.toggle_pass_btn);
+
+        pass_et=(EditText) findViewById(R.id.password_text_input);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,5 +62,27 @@ public class MainActivity extends AppCompatActivity {
     public void open_home_activity() {
         Intent intent = new Intent(this, listOfPostsClass.class);
         startActivity(intent);
+    }
+
+    public void onToggleClick (View v)
+    {
+        if (toggle_btn.isChecked())
+        {
+            pass_et.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+            Drawable img = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                img = getDrawable(R.drawable.toggle_on);
+            }
+            toggle_btn.setBackground(img);
+        }
+        else
+        {
+            pass_et.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            Drawable img = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                img = getDrawable(R.drawable.toggle_off);
+            }
+            toggle_btn.setBackground(img);
+        }
     }
 }
