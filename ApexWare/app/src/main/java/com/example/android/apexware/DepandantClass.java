bdf0cfb3f0323105b088ca32b1097142e9c3e634
[sha3er97] <37841787+sha3er97@users.noreply.github.com>
2019-03-20 17:00:02 +0200

    Create DepandantClass.java

---
+++ ApexWare/app/src/main/java/com/example/android/apexware/DepandantClass.java
@@ -0,0 +1,17 @@
+package com.example.android.apexware;
+
+public class DepandantClass {
+
+    private final DataSupplier mSupplier;
+
+    public DepandantClass(DataSupplier dataSupplier) {
+        mSupplier = dataSupplier;
+    }
+    boolean login (String username ,String password)
+    {
+        return mSupplier.login_interface(username,password);
+
+    }
+    // msupplier can be used at this point to call any function there in a generic use
+
+}
