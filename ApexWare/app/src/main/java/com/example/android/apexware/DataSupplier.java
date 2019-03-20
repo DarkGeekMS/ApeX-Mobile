06153726a2f24ca20408d34e17d5f3fe74243f87
[sha3er97] <37841787+sha3er97@users.noreply.github.com>
2019-03-20 17:00:41 +0200

    Revert "[new] added mock up service to simulate backend responses"
    This reverts commit 01dee1f915a024b72cc956a8d7d0d8a6f9a75e1a.

--- ApexWare/app/src/main/java/com/example/android/apexware/DataSupplier.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.apexware;
-
-public interface DataSupplier {
-
-    // return true if login successful and false if failed
-     boolean login_interface (String username ,String password);
-
-}
