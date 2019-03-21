9314bbfacd0aa5bf44af9c1e95eedd646021b459
[sha3er97] <37841787+sha3er97@users.noreply.github.com>
2019-03-21 15:54:58 +0200

    [new] adding tests and documentation
    unit testing for mock service and documentation comments

--- ApexWare/app/src/main/java/com/example/android/apexware/MockRestService.java
+++ ApexWare/app/src/main/java/com/example/android/apexware/MockRestService.java
@@ -1,12 +1,39 @@
 package com.example.android.apexware;
 
-// simulation to the back end service that sends dummy objects to debug
+/**
+ * this class is a simulation to the back end service that sends dummy objects and responses to the
+ * activities to debug without getting affected with the bacck end and connection proplems
+ *
+ * @author mostafa
+ */
 public class MockRestService implements DataSupplier {
 
-    @Override
-    public boolean login_interface(String username, String password) {
-        if (username.equals("")&& password.equals(""))
-            return false;
-        return true;
-    }
+  /**
+   * this function only checks if rhe sent parameters are empty strings or no and whatever their
+   * values are it returns true
+   *
+   * @param username : the user name field in the form
+   * @param password : the password field in the form
+   * @return : true if the parameters are not empty strings
+   */
+  @Override
+  public boolean login_interface(String username, String password) {
+    if (username.equals("") || password.equals("")) return false;
+    return true;
+  }
+
+  /**
+   * this function only checks if rhe sent parameters are empty strings or no and whatever their
+   * values are it returns true
+   *
+   * @param username : the user name field in the form
+   * @param email : the user email field in the form
+   * @param password : the password field in the form
+   * @return true if the parameters are not empty strings
+   */
+  @Override
+  public boolean signup_interface(String username, String email, String password) {
+    if (username.equals("") || password.equals("") || email.equals(" ")) return false;
+    return true;
+  }
 }
